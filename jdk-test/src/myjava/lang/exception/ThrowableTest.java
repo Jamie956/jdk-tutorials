cb791fe2a0b7768a403a243f345b0240ceefef4a
Jamie956 <752492509@qq.com>
2022-02-25 08:14:35 +0800

    Throwable

--- jdk-test/src/myjava/lang/exception/ThrowableTest.java
+++ jdk-test/src/myjava/lang/exception/ThrowableTest.java
@@ -18,7 +18,6 @@ public class ThrowableTest {
     @Test
     public void cons3() throws Throwable {
         Throwable b = new Throwable("exp b");
-
         Throwable a = new Throwable("exp a", b);
         throw a;
     }
@@ -29,5 +28,53 @@ public class ThrowableTest {
         Throwable a = new Throwable(b);
         throw a;
     }
+
+    @Test
+    public void getMessage() throws Throwable {
+        Throwable a = new Throwable("ops");
+        String m = a.getMessage();
+        throw a;
+    }
+
+    @Test
+    public void getLocalizedMessage() throws Throwable {
+        Throwable a = new Throwable("ops");
+        String m = a.getLocalizedMessage();
+        throw a;
+    }
+
+    @Test
+    public void getCause() throws Throwable {
+        Throwable b = new Throwable("exp b");
+        Throwable a = new Throwable("exp a", b);
+        Throwable c = a.getCause();
+        throw a;
+    }
+
+    @Test
+    public void initCause() throws Throwable {
+        Throwable b = new Throwable("exp b");
+        Throwable a = new Throwable("exp a");
+        Throwable r = a.initCause(b);
+        throw a;
+    }
+
+    @Test
+    public void toString1() throws Throwable {
+        Throwable a = new Throwable("exp a");
+        String s = a.toString();
+        throw a;
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
