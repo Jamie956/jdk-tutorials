87c0522471f96003d078dd22cb54149697c10a9e
Jamie956 <752492509@qq.com>
2022-03-03 15:06:34 +0800

    Selector

--- jdk-test/src/myjava/nio/SelectorTest.java
+++ jdk-test/src/myjava/nio/SelectorTest.java
@@ -123,4 +123,75 @@ public class SelectorTest {
             }
         }
     }
+
+    //---------------------------------------
+    @Test
+    public void server2() {
+        try {
+            ServerSocketChannel ssc = ServerSocketChannel.open();
+            ssc.socket().bind(new InetSocketAddress("127.0.0.1", 8000));
+            ssc.configureBlocking(false);
+
+            Selector selector = Selector.open();
+            ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+            ByteBuffer readBuff = ByteBuffer.allocate(1024);
+            ByteBuffer writeBuff = ByteBuffer.allocate(128);
+            writeBuff.put("received".getBytes());
+            writeBuff.flip();
+
+            while (true) {
+                int nReady = selector.select();
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> it = keys.iterator();
+
+                while (it.hasNext()) {
+                    SelectionKey key = it.next();
+                    it.remove();
+
+                    if (key.isAcceptable()) {
+                        SocketChannel socketChannel = ssc.accept();
+                        socketChannel.configureBlocking(false);
+                        socketChannel.register(selector, SelectionKey.OP_READ);
+                    } else if (key.isReadable()) {
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        readBuff.clear();
+                        socketChannel.read(readBuff);
+                        readBuff.flip();
+                        System.out.println("received : " + new String(readBuff.array()));
+                        key.interestOps(SelectionKey.OP_WRITE);
+                    } else if (key.isWritable()) {
+                        writeBuff.rewind();
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        socketChannel.write(writeBuff);
+                        key.interestOps(SelectionKey.OP_READ);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void client2() {
+        try {
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 8000));
+
+            ByteBuffer writeBuffer = ByteBuffer.allocate(32);
+            ByteBuffer readBuffer = ByteBuffer.allocate(32);
+
+            writeBuffer.put("hello".getBytes());
+            writeBuffer.flip();
+
+            while (true) {
+                writeBuffer.rewind();
+                socketChannel.write(writeBuffer);
+                readBuffer.clear();
+                socketChannel.read(readBuffer);
+            }
+        } catch (IOException e) {
+        }
+    }
 }
